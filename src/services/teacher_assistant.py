d2916f2cf991a10f73ed80eafadecdee6dd5c696
Miguel Meza <miguelisrael.meza@bbva.com>
2024-10-19 17:46:31 -0600

    fix: TeacherAssistantService method update

--- src/services/teacher_assistant.py
+++ src/services/teacher_assistant.py
@@ -5,11 +5,12 @@ class TeacherAssistantService:
 
     def __init__(self):
 
-        self.language = None
+        self.language = "es"
         self.week = None
         self.material_state = None
-        self.week = None
         self.utterance = None
+        self.next_material = False
+        self.memory = None
 
     def predict(self):
 
