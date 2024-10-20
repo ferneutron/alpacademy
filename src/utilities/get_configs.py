b4fece81b9e6286a83ab018543107577932c24e0
Miguel Meza <miguelisrael.meza@bbva.com>
2024-10-20 07:22:10 -0600

    fix: fix notebook to test backend RESTful methods

--- src/utilities/get_configs.py
+++ src/utilities/get_configs.py
@@ -6,3 +6,9 @@ def get_config():
     config = yaml.safe_load(open("src/services/config.yml"))
 
     return config
+
+def get_config_services():
+
+    config = yaml.safe_load(open("config.yml"))
+
+    return config
